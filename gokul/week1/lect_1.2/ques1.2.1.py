3b024543ff710aec165ef69448e4466440f5d971
Dharshika <dharshikag2006@gmail.com>
2026-06-23 10:44:02 +0530

    Fix formatting and add missing newline in pattern generation examples

--- gokul/week1/lect_1.2/ques1.2.1.py
+++ gokul/week1/lect_1.2/ques1.2.1.py
@@ -8,3 +8,14 @@
 #     *********#
 #     i don't know what to type
 #     ```
+
+
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+print("******")
+print("*******")
+print("*********#")
+print("i don't know what to type")
