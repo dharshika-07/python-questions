84c0788e29c65bcd68c1be6f3a345be0a521d221
Dharshika <dharshikag2006@gmail.com>
2026-06-23 12:08:36 +0530

    Update 1.4.9.py to clarify string repetition example and ensure proper formatting

--- gokul/week1/lect-1.4/1.4.9.py
+++ gokul/week1/lect-1.4/1.4.9.py
@@ -1,4 +1,6 @@
 # 9) Take an input from command line as "GOKUL" and store it in a variable 'a' , what will be the output?
 # ```
 # LINE-1
-# print(a*2)
+# print(a*2)"string" * number → repeats the string
+a= "GOKUL"
+print(a*2)#  "string" *number → repeats the string
